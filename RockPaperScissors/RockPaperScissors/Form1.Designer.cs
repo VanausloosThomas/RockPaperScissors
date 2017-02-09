0000b9b9a9f784344fdfd0f169edf3bdffb7cfb3
Steven Excelmans <steven.excelmans@cegeka.com>
2017-02-09 11:37:25 +0100

    UI

--- RockPaperScissors/RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -23,17 +23,28 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ChoiceRock = new System.Windows.Forms.Button();
             this.ChoicePaper = new System.Windows.Forms.Button();
             this.ChoiceScissors = new System.Windows.Forms.Button();
-            this.computer_choice = new System.Windows.Forms.Button();
+            this.buttonComputersChoice = new System.Windows.Forms.Button();
+            this.text_player = new System.Windows.Forms.Label();
+            this.text_computer = new System.Windows.Forms.Label();
+            this.multiplayerµ = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonPlayersChoice = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vs = new System.Windows.Forms.ImageList(this.components);
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +56,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(395, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(399, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -57,6 +68,21 @@
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // optionsToolStripMenuItem
             // 
@@ -77,28 +103,14 @@
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.aboutToolStripMenuItem.Text = "About...";
-            // 
-            // newGameToolStripMenuItem
-            // 
-            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
-            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.newGameToolStripMenuItem.Text = "New game";
-            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
             // ChoiceRock
             // 
             this.ChoiceRock.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ChoiceRock.Location = new System.Drawing.Point(12, 200);
+            this.ChoiceRock.Location = new System.Drawing.Point(12, 378);
             this.ChoiceRock.Name = "ChoiceRock";
-            this.ChoiceRock.Size = new System.Drawing.Size(120, 120);
+            this.ChoiceRock.Size = new System.Drawing.Size(120, 50);
             this.ChoiceRock.TabIndex = 1;
             this.ChoiceRock.Text = "Rock";
             this.ChoiceRock.UseVisualStyleBackColor = true;
@@ -107,9 +119,9 @@
             // ChoicePaper
             // 
             this.ChoicePaper.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ChoicePaper.Location = new System.Drawing.Point(138, 200);
+            this.ChoicePaper.Location = new System.Drawing.Point(138, 378);
             this.ChoicePaper.Name = "ChoicePaper";
-            this.ChoicePaper.Size = new System.Drawing.Size(120, 120);
+            this.ChoicePaper.Size = new System.Drawing.Size(120, 50);
             this.ChoicePaper.TabIndex = 2;
             this.ChoicePaper.Text = "Paper";
             this.ChoicePaper.UseVisualStyleBackColor = true;
@@ -118,30 +130,132 @@
             // ChoiceScissors
             // 
             this.ChoiceScissors.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ChoiceScissors.Location = new System.Drawing.Point(264, 200);
+            this.ChoiceScissors.Location = new System.Drawing.Point(264, 378);
             this.ChoiceScissors.Name = "ChoiceScissors";
-            this.ChoiceScissors.Size = new System.Drawing.Size(120, 120);
+            this.ChoiceScissors.Size = new System.Drawing.Size(120, 50);
             this.ChoiceScissors.TabIndex = 3;
             this.ChoiceScissors.Text = "Scissors";
             this.ChoiceScissors.UseVisualStyleBackColor = true;
             this.ChoiceScissors.Click += new System.EventHandler(this.button_click);
             // 
-            // computer_choice
+            // buttonComputersChoice
             // 
-            this.computer_choice.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.computer_choice.ForeColor = System.Drawing.Color.Chartreuse;
-            this.computer_choice.Location = new System.Drawing.Point(138, 50);
-            this.computer_choice.Name = "computer_choice";
-            this.computer_choice.Size = new System.Drawing.Size(120, 120);
-            this.computer_choice.TabIndex = 4;
-            this.computer_choice.UseVisualStyleBackColor = true;
+            this.buttonComputersChoice.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonComputersChoice.ForeColor = System.Drawing.Color.Chartreuse;
+            this.buttonComputersChoice.Location = new System.Drawing.Point(138, 113);
+            this.buttonComputersChoice.Name = "buttonComputersChoice";
+            this.buttonComputersChoice.Size = new System.Drawing.Size(120, 80);
+            this.buttonComputersChoice.TabIndex = 4;
+            this.buttonComputersChoice.UseVisualStyleBackColor = true;
+            // 
+            // text_player
+            // 
+            this.text_player.AutoSize = true;
+            this.text_player.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_player.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.text_player.Location = new System.Drawing.Point(174, 348);
+            this.text_player.Name = "text_player";
+            this.text_player.Size = new System.Drawing.Size(43, 17);
+            this.text_player.TabIndex = 5;
+            this.text_player.Text = "Player";
+            // 
+            // text_computer
+            // 
+            this.text_computer.AutoSize = true;
+            this.text_computer.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_computer.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.text_computer.Location = new System.Drawing.Point(145, 93);
+            this.text_computer.Name = "text_computer";
+            this.text_computer.Size = new System.Drawing.Size(119, 17);
+            this.text_computer.TabIndex = 6;
+            this.text_computer.Text = "COMPUTER OF MP";
+            this.text_computer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // multiplayerµ
+            // 
+            this.multiplayerµ.Location = new System.Drawing.Point(12, 202);
+            this.multiplayerµ.Name = "multiplayerµ";
+            this.multiplayerµ.Size = new System.Drawing.Size(80, 56);
+            this.multiplayerµ.TabIndex = 8;
+            this.multiplayerµ.Text = "Go multiplayer";
+            this.multiplayerµ.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button1.Location = new System.Drawing.Point(264, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button2.Location = new System.Drawing.Point(138, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 50);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button3.Location = new System.Drawing.Point(12, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 50);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlayersChoice
+            // 
+            this.buttonPlayersChoice.Enabled = false;
+            this.buttonPlayersChoice.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlayersChoice.ForeColor = System.Drawing.Color.Chartreuse;
+            this.buttonPlayersChoice.Location = new System.Drawing.Point(138, 265);
+            this.buttonPlayersChoice.Name = "buttonPlayersChoice";
+            this.buttonPlayersChoice.Size = new System.Drawing.Size(120, 80);
+            this.buttonPlayersChoice.TabIndex = 12;
+            this.buttonPlayersChoice.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.ImageKey = "(none)";
+            this.label1.Location = new System.Drawing.Point(170, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 39);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "VS";
+            // 
+            // vs
+            // 
+            this.vs.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("vs.ImageStream")));
+            this.vs.TransparentColor = System.Drawing.Color.Black;
+            this.vs.Images.SetKeyName(0, "Vs.pn_.png");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 381);
-            this.Controls.Add(this.computer_choice);
+            this.ClientSize = new System.Drawing.Size(399, 438);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPlayersChoice);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.multiplayerµ);
+            this.Controls.Add(this.text_computer);
+            this.Controls.Add(this.text_player);
+            this.Controls.Add(this.buttonComputersChoice);
             this.Controls.Add(this.ChoiceScissors);
             this.Controls.Add(this.ChoicePaper);
             this.Controls.Add(this.ChoiceRock);
@@ -150,6 +264,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Rock Paper Scissors";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -169,7 +284,16 @@
         private System.Windows.Forms.Button ChoiceRock;
         private System.Windows.Forms.Button ChoicePaper;
         private System.Windows.Forms.Button ChoiceScissors;
-        private System.Windows.Forms.Button computer_choice;
+        private System.Windows.Forms.Button buttonComputersChoice;
+        private System.Windows.Forms.Label text_player;
+        private System.Windows.Forms.Label text_computer;
+        private System.Windows.Forms.Button multiplayerµ;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonPlayersChoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList vs;
     }
 }
 
--- RockPaperScissors/RockPaperScissors/Form1.cs
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RockPaperScissors.Source.Service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,36 +8,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RockPaperScissors.Source.Domain;
 
 namespace RockPaperScissors {
-    public partial class Form1 : Form {
+
+    public partial class Form1 : Form, IGameservice {
+
         public Form1() {
             InitializeComponent();
         }
 
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e) {
-            //StartGame();
+            ChoicePaper.Enabled = true;
+            ChoiceRock.Enabled = true;
+            ChoiceScissors.Enabled = true;
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
             Application.Exit();
         }
 
-        private String button_click(object sender, EventArgs e) {
+        public void DefaultStartUp() {
+            buttonComputersChoice.Enabled = false;
+            text_computer.Text = "Computer";
+        }
+
+        private void button_click(object sender, EventArgs e) {
             ChoicePaper.Enabled = false;
             ChoiceRock.Enabled = false;
             ChoiceScissors.Enabled = false;
             String ChoicePlayer = "";
 
+            ComputerMakesMove();
+
             Button button = (Button)sender;
 
             if (button == ChoiceRock) {
                 ChoicePlayer = ChoiceRock.Text;
+                buttonPlayersChoice.Text = ChoiceRock.Text;
             } else if (button == ChoicePaper) {
                 ChoicePlayer = ChoicePaper.Text;
+                buttonPlayersChoice.Text = ChoicePaper.Text;
             } else if (button == ChoiceScissors) {
                 ChoicePlayer = ChoiceScissors.Text;
-            } return ChoicePlayer;
+                buttonPlayersChoice.Text = ChoiceScissors.Text;
+            } 
+        }
+
+        public void ComputerMakesMove() {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            DefaultStartUp();
+        }
+
+        private void CheckWinner() {
+            
+        }
+
+        public Winner SinglePlayerGame(Hand hand) {
+            throw new NotImplementedException();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+            MessageBox.Show("Made by team (at least) one bridge to far ...\nSeppe, Steven, Thomas & Xan\nVersion 0.1", "About");
         }
     }
 }
