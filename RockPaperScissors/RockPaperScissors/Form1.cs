e94de87a7bd71aa592b14cbcf0ab0358f491a489
Steven Excelmans <steven.excelmans@cegeka.com>
2017-02-09 16:46:33 +0100

    :rage: fixed form

--- RockPaperScissors/RockPaperScissors/Form1.cs
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -19,12 +19,12 @@ namespace RockPaperScissors {
             InitializeComponent();
         }
 
-        private void newGameToolStripMenuItem_Click(object sender, EventArgs e) {
-            ChoicePaper.Enabled = true;
-            ChoiceRock.Enabled = true;
-            ChoiceScissors.Enabled = true;
-            buttonComputersChoice.Text = "";
-            buttonPlayersChoice.Text = "";
+        private void newSinglePlayerGameToolStripMenuItem_Click(object sender, EventArgs e) {
+            ResetButtons();
+
+            count_draws.Text = "0";
+            score_player.Text = "0";
+            score_otherplayer.Text = "0";
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
@@ -37,43 +37,53 @@ namespace RockPaperScissors {
         }
 
         private void button_click(object sender, EventArgs e) {
+            DisableButtons();
+            Button button = (Button)sender;
+            CheckWinner(ref button);
+            ResetButtons();
+        }
+
+        public void ResetButtons() {
+            ChoicePaper.Enabled = true;
+            ChoiceRock.Enabled = true;
+            ChoiceScissors.Enabled = true;
+            buttonComputersChoice.Text = "";
+            buttonPlayersChoice.Text = "";
+        }
+
+        public void DisableButtons() {
             ChoicePaper.Enabled = false;
             ChoiceRock.Enabled = false;
             ChoiceScissors.Enabled = false;
-
-            Button button = (Button)sender;
-
-
-            CheckWinner(ref button);
         }
 
         private void Form1_Load(object sender, EventArgs e) {
             DefaultStartUp();
         }
 
-        internal void CheckWinner(ref Button button)
-        {
+        internal void CheckWinner(ref Button button) {
             var matchResult = gameService.SinglePlayerGame(DetermineHandChoice(button));
             buttonComputersChoice.Text = matchResult.OtherPlayerHand().ToString();
 
-            if (!matchResult.Draw)
-            {
-                MessageBox.Show(matchResult.WinnerName() + " won with " + matchResult.WinnerHand() +"!", matchResult.WinnerName() + " has won!");
-            } else
-            {
+            if (!matchResult.Draw) {
+                MessageBox.Show(matchResult.WinnerName() + " won with " + matchResult.WinnerHand() + "!", matchResult.WinnerName() + " has won!");
+            } else {
                 MessageBox.Show("Draw! Try again!", "Draw!");
             }
-
         }
 
-        private Hand DetermineHandChoice(Button button)
-        {
-            if (button == ChoicePaper)
-            {
+        /*private void Counters() {
+            count_draws.Text = matchResult.;
+            score_player.Text;
+            score_otherplayer.Text ;
+            
+        }*/
+
+        private Hand DetermineHandChoice(Button button) {
+            if (button == ChoicePaper) {
                 buttonPlayersChoice.Text = ChoicePaper.Text;
                 return Hand.Paper;
-            } else if (button == ChoiceScissors)
-            {
+            } else if (button == ChoiceScissors) {
                 buttonPlayersChoice.Text = ChoiceScissors.Text;
                 return Hand.Scissors;
             }
@@ -81,8 +91,8 @@ namespace RockPaperScissors {
             return Hand.Rock;
         }
 
-
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+
             MessageBox.Show("Made by team (at least) one bridge to far ...\nSeppe, Steven, Thomas & Xan\nVersion 0.1", "About");
         }
 
